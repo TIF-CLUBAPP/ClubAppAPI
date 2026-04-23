3f01a132c9d8b68b8b82ba7b3fbbf656d288023c
IvoBertoni89 <bertoniivox@gmail.com>
2026-04-23 17:26:59 -0300

    activity complete
    i complete activity basics functions

--- src/Application/Interfaces/IActivityService.cs
+++ src/Application/Interfaces/IActivityService.cs
@@ -6,10 +6,13 @@ public interface IActivityService
 {
     // Obtener todas las actividades (para mostrar en React)
     Task<IEnumerable<ActivityDto>> GetAllAvailableActivitiesAsync();
-    
+
     // Crear una nueva (el ABM que pidió el profe)
     Task<bool> CreateActivityAsync(ActivityDto activityDto);
-    
+
     // Inscribir a un socio (Lógica de negocio)
     Task<bool> EnrollMemberAsync(int userId, int activityId);
+
+    Task<bool> UpdateActivityAsync(int activityId, ActivityDto activityDto);
+    Task<bool> DeleteActivityAsync(int activityId);
 }
